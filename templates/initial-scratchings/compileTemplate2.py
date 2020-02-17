542f741129d05400e9a28ae0e5af5a379bd8bd8a
Shen Loh <shen@30.144.dhcp.conference.apricot.net>
2020-02-17 14:40:54 +1100

    Added another compileTemplate script

---
+++ templates/initial-scratchings/compileTemplate2.py
@@ -0,0 +1,16 @@
+from jinja2 import Template
+import yaml
+
+template_file = 'Junos-scratchings'
+variable_file = 'variableExample.yml'
+
+with open(variable_file) as file:
+  var = yaml.load(file, Loader=yaml.FullLoader)
+
+# Loading Template
+with open(template_file) as file:
+  data = file.read()
+template = Template(data)
+
+# render template
+print(template.render(var))
