d14eac4c23868ac02823422b45d7d4e844671225
Nick Pratley <nick@ix.asn.au>
2020-02-17 18:20:11 +1100

    formatting

--- examples/print_config.py
+++ examples/print_config.py
@@ -1,28 +1,33 @@
 import requests
-import sys
+
 
 def send_get(handle):
     url = "https://fierce-garden-89071.herokuapp.com/" + handle
     payload = {}
-    headers = {
-      'Authorization': 'Basic YWxjaGVteTphbGNoZW15'
-    }
-    return requests.request("GET", url, headers=headers, data = payload)
+    headers = {"Authorization": "Basic YWxjaGVteTphbGNoZW15"}
+    return requests.request("GET", url, headers=headers, data=payload)
+
 
 def print_config(uuid):
-    response = send_get('config/'+uuid)
-    print(response.json()['config'])
+    response = send_get("config/" + uuid)
+    print(response.json()["config"])
+
 
 def main():
-    response = send_get('device/')
-    for device in response.json()['results']:
-        uuid=device['id']
-        print('''
+    response = send_get("device/")
+    for device in response.json()["results"]:
+        uuid = device["id"]
+        print(
+            """
         ###########
         ## {0}  #
         ###########
-        '''.format(device['name']))
+        """.format(
+                device["name"]
+            )
+        )
         print_config(uuid)
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
